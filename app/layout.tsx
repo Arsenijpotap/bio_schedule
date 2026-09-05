e7fee9537a7be0138734777ac7cd83773e9d168a
Arsenij <arsenij.patap@gmail.com>
2026-09-05 21:21:33 +0300

    fix telegram webapp sdk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,19 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
-  title: "Расписание БГУ",
-  description: "Расписание БГУ в Telegram Mini App"
+	title: "Расписание БГУ",
+	description: "Расписание БГУ в Telegram Mini App",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="ru">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="ru">
+			<body>
+				<Script src="https://telegram.org/js/telegram-web-app.js?59" strategy="beforeInteractive" />
+				{children}
+			</body>
+		</html>
+	);
 }
